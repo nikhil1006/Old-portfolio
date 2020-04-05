2a0e251a370e20189315d14b7283beee922ec793
nikhilperi1006@gmail.com <nihilperi1006@gmail.com>
2020-04-06 00:13:50 +0530

    changed config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,9 +24,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Sathwik Vadlamani`,
+        short_name: `Sathwik`,
+        description: `Check out my portfolio`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
